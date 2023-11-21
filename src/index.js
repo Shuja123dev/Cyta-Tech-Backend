2237203452b443970a78a8d199542f523571a543
Shuja ud Din <shuja1339@gmail.com>
2023-11-21 21:30:19 +0500

    change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodeMialer = require("nodemailer");
+const serverless = require("serverless-http")
 
 const server = express();
 
