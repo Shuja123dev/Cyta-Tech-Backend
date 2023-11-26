b7f2dd1e8e041f86a3fe45cc8ed19d14fc2818ed
Shuja ud Din <shuja1339@gmail.com>
2023-11-26 14:15:50 +0500

    vercel config

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ server.get("/", (req, res) => {
     res.send("Home Page");
 })
 
+server.get("/test", (req, res) => {
+    res.send("test Route Working");
+})
+
 server.listen(8002, () => {
     console.log("Server Responding at PORT 8002");
 })
