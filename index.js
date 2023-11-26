31abee2b4f8c29fada38b372f312e922b573067c
Shuja ud Din <shuja1339@gmail.com>
2023-11-26 14:35:22 +0500

    send mail

--- index.js
+++ index.js
@@ -11,7 +11,46 @@ server.get("/", (req, res) => {
     res.send("Home Page");
 })
 
+const transporter = nodeMialer.createTransport({
+    service: 'outlook',
+    auth: {
+        user: "shujaa.udin@outlook.com",
+        pass: "Shuja*123"
+    }
+})
+
+function sendMail(message) {
+    const mailBody = `First Name : ${message.firstName}
+    Last Name : ${message.lastName}
+    E-mail : ${message.email}
+    Phone Number : ${message.phoneNo}
+    Comapny : ${message.company}
+    Service : ${message.service}
+    Project Details : ${message.projectDetails}
+    Budget : ${message.budget}
+    Deadline : ${message.deadline}
+    Comment : ${message.comment}`;
+
+
+    const mailInfo = {
+        from: 'shujaa.udin@outlook.com',
+        to: 'shuja1339@gmail.com',
+        subject: 'test',
+        text: mailBody,
+    }
+    transporter.sendMail(mailInfo, (error, info) => {
+        if (error) {
+            console.log("error : ", error);
+        }
+        else {
+            console.log("mail sent", info.response)
+        }
+    })
+}
+
 server.post("/send-mail", (req, res) => {
+    const mailInfo = req.body;
+    sendMail(mailInfo);
     res.send("test Route Working");
 })
 
